ae8f4dbac2f5ca6f708d2b1b8428853aadb1ae58
Kyle <kyleastark@gmail.com>
2025-10-05 12:52:54 -0700

    Adding all the site files

---
+++ scripts/script.js
@@ -0,0 +1,25 @@
+var menuButton = document.querySelector('.menu-button'),
+    menuIcon = document.querySelector('.menu'),    headerNav = document.querySelector('.header-nav'),
+    body = document.body;
+
+menuButton.addEventListener('click', function(){
+    if (menuIcon.classList.contains('close-icon')) {
+        headerNav.classList.add('mobile-closed');
+        headerNav.classList.remove('mobile-open');
+        menuIcon.classList.remove('close-icon');
+        body.classList.remove('menu-open');
+    } else {
+        headerNav.classList.remove('mobile-closed');
+        headerNav.classList.add('mobile-open');
+        menuIcon.classList.add('close-icon');
+        body.classList.add('menu-open');
+    }
+});
+
+$(document).ready(function(){
+    $('.global-slideshow').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false
+    });
+});
